a86ee5f47a3ee99751cda1e3a0814324dfb03858
RipKowai <georgi.damyanov@edu.futuregames.nu>
2023-11-27 20:58:16 +0100

    Simple Sorting Algorithms Done
    Bubble, Selection, Insertion done

---
+++ DataStructures Assigment/SortingAlgorithms.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Monster.h"
+#pragma region InsertionSort
+void InsertionSort(Monster monsters[], int length) {
+	for (int i = 0; i < length; i++) {
+		int key = monsters[i].dextirity;
+		int j = i - 1;
+		while (j >= 0 && monsters[j].dextirity > key) {
+			monsters[j + 1].dextirity = monsters[j].dextirity;
+			j = j - 1;
+		}
+		monsters[j + 1].dextirity = key;
+	}
+}
+
+#pragma endregion
+
+#pragma region SelectionSort
+void SelectionSort(Monster monsters[], int length) {
+    for (int i = 0; i < (length - 1); i++) {
+        int minPos = i;
+        for (int j = i + 1; j < length; j++) {
+            if (monsters[j].attack < monsters[minPos].attack) {
+                minPos = j;
+            }
+        }
+        if (minPos != i) {
+            std::swap(monsters[i], monsters[minPos]);
+        }
+    }
+}
+#pragma endregion
+
+#pragma region BubbleSort
+void BubbleSort(Monster monsters[], int length) {
+	bool swapped;
+	int i = 0;
+	do {
+		swapped = false;
+		for (int j = 0; j < (length - 1); j++) {
+			if (monsters[j].health > monsters[j + 1].health) {
+				int temp = monsters[j].health;
+				monsters[j].health = monsters[j + 1].health;
+				monsters[j + 1].health = temp;
+				swapped = true;
+			}
+		}
+	} while (swapped);
+}
+#pragma endregion
+
--- DataStructures Assigment/main1.cpp
+++ DataStructures Assigment/main1.cpp
@@ -1,40 +1,44 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 #include "Monster.h"
-#include "Selection Sort.h"
+#include "SortingAlgorithms.h"
+
+typedef std:: chrono::milliseconds MSEC;
+typedef std:: chrono::high_resolution_clock HRC;
 
 float randomFloat(float min, float max)
 {
     return min + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX) / (max - min));
 }
-void selection_sort(int a[], int length);
-
 
 int main()
 {
+    auto start = HRC::now();
+
     std::fstream file("AssignmentNodes.txt");
     std::string line;
     std::srand(time(0));
 
 #pragma region Monsters
 
+    const int lenght = 10000;
+    Monster monsters[lenght]{};
 
-    Monster monsters[10]{};
-
-    for (int i = 0; i < 10; i++) {
-        monsters[i].attack = rand () % 250;
+    for (int i = 0; i < lenght; i++) {
+        monsters[i].attack = rand () % 500;
         monsters[i].health = rand () % 1000;
+        monsters[i].dextirity = rand () % 100;
     }
+    
+    BubbleSort(monsters, lenght);//318msec for 10k monsters //300msec after optimization 
+    //SelectionSort(monsters, lenght);//180msec for 10k monsters
+    //InsertionSort(monsters, lenght);//74msec for 10k monsters
+    auto end = HRC::now();
+    auto exec_time = std::chrono::duration_cast<MSEC>(end - start);
 #pragma endregion
-
-#pragma region SelectionSort
-    //void selection_sort(Monster monsters[]);
-    int a[] = { 6, 5, 3, 2, 1, 4 };
-    int length = 6;
-#pragma endregion
-
 
 #pragma region Graph
 
@@ -51,17 +55,15 @@ int main()
 #pragma endregion
 
 #pragma region WriteOut
+
   
     for (Monster& m : monsters) {
-        std::cout << "Monster:" << std::endl;
-        std::cout <<"Dph:" << m.attack << std::endl;
-        std::cout <<"Health:" << m.health << std::endl;
+        std::cout <<"Monster:" << std::endl;
+        std::cout <<"Hp:" << m.health << std::endl;
+        std::cout <<"Dps:" << m.attack << std::endl;
+        std::cout <<"Dex:" << m.dextirity << std::endl;
     }
-    selection_sort(a, length);
-
-    for (int i = 0; i < length; i++) {
-        std::cout << "[" << i << "]" << a[i] << std::endl;
-    }
+    std::cout << "SortedList exec time =" << exec_time.count() <<"msec" << std::endl;
 #pragma endregion
     return 0;
 }
